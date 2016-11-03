ae00e3a86ae6f5f5a2bc385adcabd879d7be6627
rpenlington <rpenlington@icloud.com>
2016-11-02 21:42:03 -0400

    Quick Start Template Check In

---
+++ FieldPoC/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FieldPoC
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://fieldpoc.azurewebsites.net";
+	}
+}
+
